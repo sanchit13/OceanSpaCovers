7850ac9ba6f3ab9f3131081a15b4a5dab5d59123
Sanchit Dhiman <dhiman.sanchit@gmail.com>
2017-09-28 18:57:17 -0500

    debugging server.js

--- server.js
+++ server.js
@@ -7,20 +7,31 @@ const router = require("./api/");
 /*   ============ SET UP OUR WEB APPLICATION ============ */
 const app = express();
 
+
+
+
 /*   ============ TEMPLATING USING PUG ============ */
-app.set("views", __dirname + "/views");
-app.set('view engine', 'pug');
-app.set('view options', {
-  layout: false
-});
+//app.set("views", __dirname + "/views");
+//app.set('view engine', 'pug');
+//app.set('view options', {
+//  layout: false
+//});
 
 /*   ============ STATIC FILES ============ */
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 /*   ============ ROUTES ============ */
-app.use('/', router);
+//app.use('/', router);
 
 /*   ============ MAKE IT LIVE ============ */
-app.listen(8080, function () {
-  console.log("API is live listening on http://localhost:8080");
+//app.listen(8080, function () {
+//  console.log("API is live listening on http://localhost:8080");
+//})
+
+app.get('/', function (req, res) {
+  res.send('Hello Digital Ocean!')
+})
+
+app.listen(3000, function () {
+  console.log('Magic is happening on port 3000!')
 })
